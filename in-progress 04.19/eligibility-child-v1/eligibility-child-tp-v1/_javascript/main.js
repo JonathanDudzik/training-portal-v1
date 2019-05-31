bacf6d2c79ed28b94daebd0bc1a1efa03e83ec21
[Jonathan Dudzik] <[jonathan.dudzik@dhhs.nc.gov]>
2019-05-31 11:16:09 -0400

    initial commit

--- in-progress 04.19/eligibility-child-v1/eligibility-child-tp-v1/_javascript/main.js
+++ in-progress 04.19/eligibility-child-v1/eligibility-child-tp-v1/_javascript/main.js
@@ -1,3 +1,333 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('Hello Bulma!');
+/***************************************
+* ei11 Array.from & forEach polyfills
+**************************************/
+// Production steps of ECMA-262, Edition 6, 22.1.2.1
+if (!Array.from) {
+    Array.from = (function () {
+      var toStr = Object.prototype.toString;
+      var isCallable = function (fn) {
+        return typeof fn === 'function' || toStr.call(fn) === '[object Function]';
+      };
+      var toInteger = function (value) {
+        var number = Number(value);
+        if (isNaN(number)) { return 0; }
+        if (number === 0 || !isFinite(number)) { return number; }
+        return (number > 0 ? 1 : -1) * Math.floor(Math.abs(number));
+      };
+      var maxSafeInteger = Math.pow(2, 53) - 1;
+      var toLength = function (value) {
+        var len = toInteger(value);
+        return Math.min(Math.max(len, 0), maxSafeInteger);
+      };
+  
+      // The length property of the from method is 1.
+      return function from(arrayLike/*, mapFn, thisArg */) {
+        // 1. Let C be the this value.
+        var C = this;
+  
+        // 2. Let items be ToObject(arrayLike).
+        var items = Object(arrayLike);
+  
+        // 3. ReturnIfAbrupt(items).
+        if (arrayLike == null) {
+          throw new TypeError('Array.from requires an array-like object - not null or undefined');
+        }
+  
+        // 4. If mapfn is undefined, then let mapping be false.
+        var mapFn = arguments.length > 1 ? arguments[1] : void undefined;
+        var T;
+        if (typeof mapFn !== 'undefined') {
+          // 5. else
+          // 5. a If IsCallable(mapfn) is false, throw a TypeError exception.
+          if (!isCallable(mapFn)) {
+            throw new TypeError('Array.from: when provided, the second argument must be a function');
+          }
+  
+          // 5. b. If thisArg was supplied, let T be thisArg; else let T be undefined.
+          if (arguments.length > 2) {
+            T = arguments[2];
+          }
+        }
+  
+        // 10. Let lenValue be Get(items, "length").
+        // 11. Let len be ToLength(lenValue).
+        var len = toLength(items.length);
+  
+        // 13. If IsConstructor(C) is true, then
+        // 13. a. Let A be the result of calling the [[Construct]] internal method 
+        // of C with an argument list containing the single item len.
+        // 14. a. Else, Let A be ArrayCreate(len).
+        var A = isCallable(C) ? Object(new C(len)) : new Array(len);
+  
+        // 16. Let k be 0.
+        var k = 0;
+        // 17. Repeat, while k < len… (also steps a - h)
+        var kValue;
+        while (k < len) {
+          kValue = items[k];
+          if (mapFn) {
+            A[k] = typeof T === 'undefined' ? mapFn(kValue, k) : mapFn.call(T, kValue, k);
+          } else {
+            A[k] = kValue;
+          }
+          k += 1;
+        }
+        // 18. Let putStatus be Put(A, "length", len, true).
+        A.length = len;
+        // 20. Return A.
+        return A;
+      };
+    }());
+  }
+  
+  // Production steps of ECMA-262, Edition 5, 15.4.4.18
+  // Reference: http://es5.github.io/#x15.4.4.18
+  if (!Array.prototype.forEach) {
+  
+    Array.prototype.forEach = function(callback/*, thisArg*/) {
+  
+      var T, k;
+  
+      if (this == null) {
+        throw new TypeError('this is null or not defined');
+      }
+  
+      // 1. Let O be the result of calling toObject() passing the
+      // |this| value as the argument.
+      var O = Object(this);
+  
+      // 2. Let lenValue be the result of calling the Get() internal
+      // method of O with the argument "length".
+      // 3. Let len be toUint32(lenValue).
+      var len = O.length >>> 0;
+  
+      // 4. If isCallable(callback) is false, throw a TypeError exception. 
+      // See: http://es5.github.com/#x9.11
+      if (typeof callback !== 'function') {
+        throw new TypeError(callback + ' is not a function');
+      }
+  
+      // 5. If thisArg was supplied, let T be thisArg; else let
+      // T be undefined.
+      if (arguments.length > 1) {
+        T = arguments[1];
+      }
+  
+      // 6. Let k be 0.
+      k = 0;
+  
+      // 7. Repeat while k < len.
+      while (k < len) {
+  
+        var kValue;
+  
+        // a. Let Pk be ToString(k).
+        //    This is implicit for LHS operands of the in operator.
+        // b. Let kPresent be the result of calling the HasProperty
+        //    internal method of O with argument Pk.
+        //    This step can be combined with c.
+        // c. If kPresent is true, then
+        if (k in O) {
+  
+          // i. Let kValue be the result of calling the Get internal
+          // method of O with argument Pk.
+          kValue = O[k];
+  
+          // ii. Call the Call internal method of callback with T as
+          // the this value and argument list containing kValue, k, and O.
+          callback.call(T, kValue, k, O);
+        }
+        // d. Increase k by 1.
+        k++;
+      }
+      // 8. return undefined.
+    };
+  }
+
+  /***************************************
+  * control the side menu w/ hamburger
+  **************************************/
+  document.addEventListener('DOMContentLoaded', () => {
+
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+  
+      // Add a click event on each of them
+      $navbarBurgers.forEach( el => {
+        el.addEventListener('click', () => {
+  
+          // Get the target from the "data-target" attribute
+          const target = el.dataset.target;
+          const $target = document.getElementById(target);
+  
+          // Toggle the "is-hidden-touch" class on "sideMenu" and is-active on "navbar-burger".
+          el.classList.toggle('is-active');
+          $target.classList.toggle('is-hidden-touch');
+  
+        });
+      });
+    }
+  
+  });
+
+/***************************************
+* Side-menu hiding/unhiding sections
+**************************************/
+// get all elements as an array that will act as a selector
+const selectors = Array.from(document.querySelectorAll('[data-selector]'));
+
+// get all elements that are sections
+const sections = Array.from(document.querySelectorAll('[data-section]'));
+
+// give each selector an event listener, remove active class, and
+selectors.forEach(function(selector) {
+    selector.addEventListener('click', toggleSections);
+});
+
+function toggleSections(e) {
+
+    // selected section
+    const section = document.querySelector(`[data-section="${e.target.dataset.selector}"]`);
+
+    // hide all sections
+    sections.forEach(function(section) {
+        section.classList.add('is-display-none');
+    });
+
+    // unhide selected section
+    section.classList.remove('is-display-none');
+
+    // remove active class from all selectors
+    selectors.forEach(function(selector) {
+        selector.classList.remove('is-active');
+    });
+
+    // make current selector active class
+    e.target.classList.add('is-active');
+
+}
+
+/***************************************
+ * Quiz Logic
+ **************************************/
+var vmQuiz = new Vue({
+    el: "#multi-choice-quiz-vue",
+    
+    data: {
+        currentQuestion: 0,
+        totalScore: 0,
+        showModalClass: false,
+        quizModalHeading: 'That is incorrect.',
+        quizModalBody: '',
+        hideQuizContent: false,
+        hideRetakeBtn: true,
+        hideCertBtn: true,
+
+        // ARRAY OF QUESTION OBJECTS
+        allQuestions: [
+            {
+                question: "Out-side-school-hours care centers are required to enroll participants with CACFP enrollment documentation under which circumstance?",
+                choices: ["The institution is under a sponsoring organization", "The out-side-school-hours care program is part of the center’s CACFP", "The out-side-school-hours care center has an occupancy limit over 100 persons"],
+                explanation: "Remember, if an outside-school-hours care center is operated as part of a childcare center that participates in the CACFP, it will require CACFP enrollment documentation.",
+                correct: 1
+            },
+            {
+                question: "For how long is child enrollment documentation valid?",
+                choices: ["3 years plus the current year", "One year from the date that it was signed by the parent or guardian", "For as long as the child is continuously enrolled in the center"],
+                explanation: "Remember, child enrollment documentation is valid for one year from the date that it was signed by the parent or guardian.",
+                correct: 1
+            },
+            {
+                question: "Which of the following is a consequence of missing or incomplete enrollment documentation?",
+                choices: ["Your institution will be required to retake this training", "Your institution will be required to produce proper enrollment documentation within 30 days", "There will be reimbursement disallowances"],
+                explanation: "Missing or incomplete enrollment documentation could cost the center money as reimbursement is disallowed for participants without proper enrollment documentation.",
+                correct: 2
+            },
+            {
+                question: "Which of the following describes information that must be recorded on enrollment documentation?",
+                choices: ["Parent’s or guardian’s signature, days and hours of care, and meals normally received", "Participant’s date of birth and family’s household income", "The participant’s ethnic and racial data and any dietary restrictions"],
+                explanation: "Enrollment documentation must include information on each child's normal days and hours of care, information on meals normally received while in care, and be signed by a parent or guardian.",
+                correct: 0
+            },
+            {
+                question: "What is the best practice when a child withdraws from the CACFP?",
+                choices: ["Document the participant withdrawal date on the enrollment documentation", "Destroy the enrollment form to keep it confidential", "Send the enrollment form to the State agency explaining the withdrawal"],
+                explanation: "When a child withdraws, write the participant’s withdrawal date on the enrollment documentation and then re-file the documentation.",
+                correct: 0
+            },
+            {
+                question: "How long must enrollment documentation be kept on file?",
+                choices: ["One year after the participant withdraws", "30 days after the participant withdraws", "3 years plus the current year"],
+                explanation: "Enrollment documentation must be maintained on file for 3 years plus the current year. Additionally, it is best practice to have a policy in place that specifies how the institution will retain the records for the required 3 years plus the current year.",
+                correct: 2
+            }
+        ],
+    },
+
+    computed: {
+        questionTxt() {
+          return this.allQuestions[this.currentQuestion].question
+        },
+        choice1() {
+          return this.allQuestions[this.currentQuestion].choices[0];
+        },
+        choice2() {
+          return this.allQuestions[this.currentQuestion].choices[1];
+        },
+        choice3() {
+          return this.allQuestions[this.currentQuestion].choices[2];
+        }
+    },
+
+    methods: {
+        quizSubmit() {
+
+            // user's selected radio button
+            const selectedChoice = document.querySelector('input[name="answer"]:checked').value;
+            
+            // all checkboxes in quiz
+            const quizCheckboxes = Array.from(document.forms["quizForm"]["answer"]);
+            
+            // display defaults in case of incorrect answer
+            if (selectedChoice !== this.allQuestions[this.currentQuestion].correct) {
+                this.quizModalHeading = 'That is incorrect.';
+                this.quizModalBody = '';
+            };
+            
+            // change feedback and increase score if correct answer
+            if (selectedChoice == this.allQuestions[this.currentQuestion].correct) {
+                this.quizModalHeading = "That is correct!";
+                this.quizModalBody = (this.allQuestions[this.currentQuestion].explanation);
+                this.totalScore ++;
+            };
+
+            // show the quiz modal with feedback
+            this.showModalClass = true;
+            
+            // do something if end of quiz has been reached
+            if (this.currentQuestion == this.allQuestions.length -1 ) {
+                this.totalScore >= (this.allQuestions.length*0.75) ? vmQuiz.hideCertBtn = false : vmQuiz.hideRetakeBtn = false;
+                this.hideQuizContent = true;
+                return
+            };
+            
+            // move to the next question (will not increment if end of quiz has been reached)
+            this.currentQuestion ++;
+
+            // goes through the form and UNchecks each answer
+            quizCheckboxes.forEach(function(quizCheckbox) {
+                quizCheckbox.checked = false;
+            }); 
+        },
+
+        reloadQuiz() {
+            this.currentQuestion = 0;
+            this.totalScore = 0;
+            this.hideQuizContent = false;
+            this.hideRetakeBtn = true;
+        }
+    }
 });
